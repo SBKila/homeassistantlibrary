d5fed25d913951a809d5d107e9911aba04fc797a
SBKILA <sblancduino@gmail.com>
2024-01-21 12:15:35 +0100

    fix logging

--- src/HALib/tools/debug.h
+++ src/HALib/tools/debug.h
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 #ifdef DEBUG_HALIB_DEVICE
-#define HALIB_DEVICE_DEBUG_MSG(...) DEBUG_MSG("MAIN", __VA_ARGS__)
-#define HALIB_ADAPTER_DEBUG_MSG(...) DEBUG_MSG("[HALIB] Adapter: ", __VA_ARGS__)
-#define HALIB_COMPONENT_DEBUG_MSG(...) DEBUG_MSG("[HALIB] Component: ", __VA_ARGS__)
-#define HALIB_NODE_DEBUG_MSG(...) DEBUG_MSG("[HALIB] Node: ", __VA_ARGS__)
+#define HALIB_DEVICE_DEBUG_MSG(...) DEBUG_MSG("HALIB", __VA_ARGS__)
+#define HALIB_ADAPTER_DEBUG_MSG(...) DEBUG_MSG("HALIB Adapter", __VA_ARGS__)
+#define HALIB_COMPONENT_DEBUG_MSG(...) DEBUG_MSG("HALIB Component", __VA_ARGS__)
+#define HALIB_NODE_DEBUG_MSG(...) DEBUG_MSG("HALIB Node", __VA_ARGS__)
 #else
 #define HALIB_DEVICE_DEBUG_MSG(...)
 #define HALIB_ADAPTER_DEBUG_MSG(...)
